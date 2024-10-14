58f03fe7b6c1ca9553fd0d2e80a7902a7d26786c
Nicolas Vero <nicolasvero03@gmail.com>
2024-10-14 15:20:45 +0200

    séparation de la création du pdf

---
+++ generator.py
@@ -0,0 +1,40 @@
+import os
+from fpdf import FPDF
+from utilities import * 
+from datas import * 
+
+def generate_pdf(props):
+
+    if not os.path.exists(SAVE_PATH):
+        os.makedirs(SAVE_PATH)
+
+
+    pdf = FPDF("P", "mm", "A4")
+    #! a voir pourquoi ne semble pas changer le pdf final 
+    # pdf.set_auto_page_break(auto=True)
+    pdf.set_auto_page_break(auto=True, margin=15)
+    # pdf.multi_cell(0, 10, corps)
+    pdf.add_page()
+
+    pdf.set_font(FONT_FAMILY, size=12)
+
+    pdf.set_xy(10, 10)
+
+    text = get_personnal_information_section()
+
+    # pdf.multi_cell(0, 10, text)
+    pdf.multi_cell(0, 10, text)
+    pdf.ln(LINE_HEIGHT)
+
+    pdf.set_font(FONT_FAMILY, "B", 12)
+    # pdf.cell(0, 10, OBJECT, ln=LINE_HEIGHT)
+
+    pdf.set_font(FONT_FAMILY, size=12)
+
+    corps = get_text_section(props)
+    pdf.multi_cell(0, 10, corps)
+
+    pdf.cell(0, 10, "Nicolas Vero")
+
+    pdf_filename = os.path.join(SAVE_PATH, formate_company_name(props.get("company")) + ".pdf")
+    pdf.output(pdf_filename)
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import os
 from fpdf import FPDF
 from datas import *
 from utilities import *
-import os
-
-
-if not os.path.exists(SAVE_PATH):
-    os.makedirs(SAVE_PATH)
+from generator import *
 
 
 company = input("Entreprise :")
@@ -18,47 +15,11 @@ if display_dut_info in ['', 'y']:
     dut_info_skills = input("DUT Info skills (separator ,) :")
 
 
-props = {
+generate_pdf({
     "company" : company,
     "spontaneous" : spontaneous,
     "job" : job,
     "main_skills" : main_skills,
     "display_dut_info" : display_dut_info,
     "dut_info_skills" : dut_info_skills if 'dut_info_skills' in globals() else None
-}
-
-
-
-
-pdf = FPDF("P", "mm", "A4")
-#! a voir pourquoi ne semble pas changer le pdf final 
-# pdf.set_auto_page_break(auto=True)
-pdf.set_auto_page_break(auto=True, margin=15)
-# pdf.multi_cell(0, 10, corps)
-pdf.add_page()
-
-pdf.set_font(FONT_FAMILY, size=12)
-
-pdf.set_xy(10, 10)
-
-text = get_personnal_information_section()
-
-# pdf.multi_cell(0, 10, text)
-pdf.multi_cell(0, 10, text)
-pdf.ln(LINE_HEIGHT)
-
-pdf.set_font(FONT_FAMILY, "B", 12)
-# pdf.cell(0, 10, OBJECT, ln=LINE_HEIGHT)
-
-pdf.set_font(FONT_FAMILY, size=12)
-
-corps = get_text_section(props)
-pdf.multi_cell(0, 10, corps)
-
-pdf.cell(0, 10, "Nicolas Vero")
-
-pdf_filename = os.path.join(SAVE_PATH, formate_company_name(company) + ".pdf")
-pdf.output(pdf_filename)
-
-
-print(formate_company_name(company))
+})
